1eb7a44acb7854ec9cc38beb822725ba8064450c
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-02-04 17:01:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,103 +1,89 @@
-import React, {useEffect, useState} from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { Todo } from './Todo';
-import axios from 'axios';
-
-const apiLink = "https://jsonplaceholder.typicode.com/todos";
+import React, { useState, useEffect } from "react";
+import "../styles/App.css";
+import { Loader } from "./Loader";
+import { Todo } from "./Todo";
 
 const App = () => {
+  const [todo, setTodos] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [showData, setShowData] = useState([]);
+  const [checkedCompleted, setCheckedCompleted] = useState(true)
+  const [checkedInCompleted, setCheckedInCompleted] = useState(true)
 
-  // const [complete, setcomplete] = useState(true);
-  const [ischecked, setischecked] = useState(true);
-  const [incomplete, setincomplete] = useState(false);
-  const [loading, setloading] = useState(false);
-  const [post, setpost] = useState([]);
-  const [get, setget] = useState([]);
-  // let first;
+  useEffect(() => {
+    getAllTodos();
+  }, []);
+
+  useEffect(() => {
+    loadFilterProducts();
+  }, [checkedCompleted, checkedInCompleted]);
+
 
   
-  useEffect(()=>{
-    
-    async function getStoredData(){
-        const response = await axios.get(apiLink);
-        console.log(response);
-        // console.log(response.data);
-        
-        setpost(response.data.splice(0, 20));
-        // console.log(...post)
-        setloading(false);
-        // const timer = setTimeout(() => {<Loader />}, 1000);
-        //   return () => clearTimeout(timer);
+  const getAllTodos = async () => {
+    setLoader(true);
+    await fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data.slice(0,20));
+        setShowData(data.slice(0,20));
+      });
+    setLoader(false);
+  };
+
+  const loadFilterProducts = () => {
+    if(checkedCompleted && checkedInCompleted){
+      let newFilter = [...todo];
+      setShowData(newFilter);
+    } else if(checkedCompleted){
+      let newFilter = [...todo];
+      newFilter = newFilter.filter((data) => data.completed)
+      setShowData(newFilter);
+      setCheckedCompleted(true)
+    } else if(!checkedCompleted){
+      let newFilter = [...todo];
+      newFilter = newFilter.filter((data) => !data.completed)
+      setShowData(newFilter);
+      setCheckedInCompleted(true)
     }
-    getStoredData();
-  }, [])
-  
-  function handleChange(e){
-    //  setcomplete(!complete);
-
-    let word = e.target.value;
-    
-    // console.log(word)
-    setincomplete(!incomplete);
-    setischecked(!ischecked);
-  
-    // if(word === "completed"){
-    //   const filtered = post.filter(item => item==="false")
-    //   setpost(filtered)
-    //   console.log(word);
-    // }
   }
-
+  
   return (
     <>
-      {loading && (
-        <h1>
-          <Loader />
-        </h1>
-      )}
-      { !loading && (
-        <>
-          {post
-          .map((e)=>(
-            
-            <Todo
-            
-                key = {e.id}
-                title = {e.title}
-                completed = {e.completed ? "completed" : "incomplete"}
-                onChange = {handleChange}
-                // toggleTaskCompleted = {toggleTaskCompleted}
-                // onClick = {() => filterItem ("Completed")}
-                // post = {getFilteredTodo()}
-                />
-            ))}
+      {loader ? <Loader /> : null}
+      <ol>
+        {showData.map(
+          (t) =>
+              <li key={t.id}>
+                {" "}
+                <Todo id={t.id} title={t.title} completed={t.completed} />{" "}
+              </li>
+        )}
+      </ol>
 
-            {/* {
-              post.filter(item => item.completed ? (setget) : (setget))
-            } */}
-            <br />
-            <br />
-              {/* <Todo id = {response.data[0].id}/> */}
-            <div id='filter-holder'>
-              <span>Show completed</span>
-                <input type="checkbox" 
-                id="completed-checkbox" 
-                checked={ischecked} 
-                value = "completed" 
-                onChange={handleChange} />
-                <br />
-                <span>Show incompleted</span>
-                <input type="checkbox" 
-                id="incompleted-checkbox" 
-                // checked={incomplete}
-                value="incompleted"
-                onChange={handleChange} /> 
-            </div>
-        </>
-      )
-      }
+      <div id="filter-holder">
+        <label htmlFor="completed-checkbox">Show completed</label>
+        <input
+          type="checkbox"
+          id="completed-checkbox"
+          name="complete"
+          value="completed"
+          checked={checkedCompleted}
+          onChange={(e) => setCheckedCompleted(e.target.checked)}
+        />{" "}
+        <br />
+        <label htmlFor="incompleted-checkbox">Show Incompleted</label>
+        <input
+          type="checkbox"
+          id="incompleted-checkbox"
+          name="incomplete"
+          value="incompleted"
+          checked={checkedInCompleted}
+          onChange={(e) => setCheckedInCompleted(e.target.checked)}
+        />
+      </div>
     </>
-  )
-  }
+  );
+};
+
 export default App;
